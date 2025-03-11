e66b52314c20f9d5a14e2afe059125b4b6b052ef
Yvonne-del <ys359375@gmail.com>
2025-03-11 21:56:51 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const name = ["Charlie", "Samip", "Ali"];
+function whiteCard(name){
+    for (let i=0; i<name.length; i++){
+        console.log(`Thank you, ${name[i]}, for the wonderful birthday gift!`)
+    }
+    return name;
+}
+
+
+function writeCards(names, event) {
+  // Create an empty array to hold the messages
+  const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      // Build the thank you message using string interpolation
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      // Add the message to the new array
+      messages.push(message);
+    }
+    return messages;
+}
+function countDown(num){
+  let i = num;
+  while(i >= 0){
+    console.log(i);
+    i--;
+  }
+}
+countDown(5)
